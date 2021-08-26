a2ff2251d504d4dcc97bf5b6a15760a572040095
Jason Edstrom <jason@instacode.io>
2021-08-25 19:09:12 -0500

    data updates

--- src/main/java/io/instacode/university/department/model/Department.java
+++ src/main/java/io/instacode/university/department/model/Department.java
@@ -11,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -19,6 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
+@ToString
 public class Department implements Serializable {
 
   @Id
--- src/test/java/io/instacode/university/department/DepartmentServiceApplicationTests.java
+++ src/test/java/io/instacode/university/department/DepartmentServiceApplicationTests.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 import io.instacode.university.department.data.Converter;
 import io.instacode.university.department.data.Course;
@@ -48,4 +51,48 @@ class DepartmentServiceApplicationTests {
     }
 
   }
+
+  @Test
+  public void updateMyData() throws IOException {
+//    File file = new File("data/newdeptscrape.csv");
+//    List<List<String>> lists = readToArray(file);
+//
+//    for (List<String> strings : lists) {
+//      String deptCode = strings.get(0);
+//      String deptName = strings.get(1);
+//
+//      Department department = departmentRepository.findDepartmentByDepartmentCode(deptCode);
+//
+//      if (department != null){
+//        department.setDepartmentName(deptName);
+//        departmentRepository.save(department);
+//       System.out.println("Department updated: " + department);
+//      } else {
+//        department = new Department(deptCode, deptName);
+//        departmentRepository.save(department);
+//        System.out.println("Department updated: " + department);
+//      }
+
+    List<Department> departmentList = departmentRepository.findAll();
+
+    for (Department department : departmentList) {
+      department.setDepartmentCode(department.getDepartmentCode().trim());
+      departmentRepository.save(department);
+    }
+
+  }
+  public List<List<String>> readToArray(File file){
+    List<List<String>> records = new ArrayList<>();
+    try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        String[] values = line.split(",");
+        records.add(Arrays.asList(values));
+      }
+  } catch (IOException e) {
+      e.printStackTrace();
+    }
+return records;
+  }
+
 }
